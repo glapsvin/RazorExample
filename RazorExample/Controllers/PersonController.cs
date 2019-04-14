772f0f54659b4d1313d27e31535cc907c9e41098
Mike <glapsvin@gmail.com>
2019-04-14 17:14:54 -0400

    initial commit of the Razor MVC example project

---
+++ RazorExample/Controllers/PersonController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorExample.Controllers
+{
+    
+    public class PersonController : Controller
+    {
+        
+        public IActionResult NewPerson()
+        {
+            return View();
+        }
+
+        public IActionResult NewPersonTag()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewPerson(Models.Person personModel)
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                 * Since the state is valid here we would do normal processing of the form (saving data in DB etc) 
+                 */
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(personModel);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult NewPersonTag(Models.Person personModel)
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                 * Since the state is valid here we would do normal processing of the form (saving data in DB etc) 
+                 */
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(personModel);
+            }
+        }
+
+        public IActionResult VerifyEmail(string email)
+        {
+            var emailList = new List<string>()
+            {
+                "x@y.com"              
+            };
+
+            if (emailList.Contains(email))
+            {
+                return Json("Email is already taken");
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ RazorExample/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorExample.Models
+{
+    public class Person
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Organization { get; set; }
+        [Required]
+        [EmailAddress]
+        [Remote("VerifyEmail", "Person")]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        
+        public Address HomeAddress { get; set; }
+        
+    }
+}
